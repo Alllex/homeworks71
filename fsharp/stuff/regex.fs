05d2daf42aa300fa41ae34163a631484bdd428db
Alex Semin <alllex.semin@gmail.com>
2013-09-13 18:49:01 +0400

    eMail address check

---
+++ fsharp/stuff/regex.fs
@@ -0,0 +1,31 @@
+open System.Text.RegularExpressions
+
+let name = "^[a-zA-Z][a-zA-Z0-9\.~!#$:\-%\^*()]{0,256}@"
+let domain2 = "(([a-zA-Z0-9\-~!#$:%\^*()]){1,30}\.)+"
+let domain1 = "(name|info|yandex|domain|museum|[a-zA-Z]{2,3})$"
+
+let check = new Regex(name + domain2 + domain1)
+
+let correct = [ "alllex.semin@gmail.com"
+                "mE9ak!11er100500@mail.ru"
+                "superman323@supersite.info"
+                "support@yandex.ru"
+                "victor.polozov@gmail.com"
+                "victorp@math.spbu.ru"
+                "victor.polozov@lanit-tercom.com" ]
+              
+let incorrect = [ "!!!!!!@gmail.com"
+                  "example@@mail.ru"
+                  "mymail.ru"
+                  "@gmail.com"
+                  "example@.ru"
+                  "example@mail.nasa"
+                  "example@verylooooooooooooooooooooooooooongDomain.com"
+                  "example@domain2..com"
+                  "info@dom.c0m" ]
+
+let test1 = List.fold (fun acc mail -> acc && (check.IsMatch(mail))) true correct
+let test2 = List.fold (fun acc mail -> acc || (check.IsMatch(mail))) false incorrect
+
+printfn "%A" test1
+printfn "%A" test2
