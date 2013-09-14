bfa3738d362e8c0b912ae2ece4480f1399b4671d
Alex Semin <Alllex.Semin@gmail.com>
2013-09-14 16:45:42 +0400

    fix some emails

--- fsharp/stuff/regex.fs
+++ fsharp/stuff/regex.fs
@@ -1,10 +1,11 @@
 open System.Text.RegularExpressions
 
-let name = "^[a-zA-Z][a-zA-Z0-9\.~!#$:\-%\^*()]{0,256}@"
+let name = "^[a-zA-Z0-9~!#$:\-%\^*()]{0,30}"
+let fullName = name + "[.]" + name + "@"
 let domain2 = "(([a-zA-Z0-9\-~!#$:%\^*()]){1,30}\.)+"
-let domain1 = "(name|info|yandex|domain|museum|[a-zA-Z]{2,3})$"
+let domain1 = "(name|info|yandex|museum|[a-zA-Z]{2,3})$"
 
-let check = new Regex(name + domain2 + domain1)
+let check = new Regex(fullName + domain2 + domain1)
 
 let correct = [ "alllex.semin@gmail.com"
                 "mE9ak!11er100500@mail.ru"
@@ -14,7 +15,7 @@ let correct = [ "alllex.semin@gmail.com"
                 "victorp@math.spbu.ru"
                 "victor.polozov@lanit-tercom.com" ]
               
-let incorrect = [ "!!!!!!@gmail.com"
+let incorrect = [ ".!!!!!!@gmail.com"
                   "example@@mail.ru"
                   "mymail.ru"
                   "@gmail.com"
@@ -22,7 +23,8 @@ let incorrect = [ "!!!!!!@gmail.com"
                   "example@mail.nasa"
                   "example@verylooooooooooooooooooooooooooongDomain.com"
                   "example@domain2..com"
-                  "info@dom.c0m" ]
+                  "info@dom.c0m"
+                  "me..my@gnail.com" ]
 
 let test1 = List.fold (fun acc mail -> acc && (check.IsMatch(mail))) true correct
 let test2 = List.fold (fun acc mail -> acc || (check.IsMatch(mail))) false incorrect
