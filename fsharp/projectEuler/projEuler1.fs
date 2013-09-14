ebd7b919bfe4ad87d910102b105d8f578fa62e11
Alex Semin <Alllex.Semin@gmail.com>
2013-09-14 17:57:55 +0400

    Remove all bad-organised code

--- fsharp/projectEuler/projEuler1.fs
+++
@@ -1,9 +0,0 @@
-(*
-    Problem: http://projecteuler.net/problem=1
-    Author: Alex Semin
-    Place: Math-Mech SPbU 171
-    Time:  03.2013
-*)
-
-printfn "%A" (List.fold (+) 0 (List.filter (fun x -> x % 3 = 0 || x % 5 = 0) [1..999]))
-
--- spring13/temp/tempstruct.h
+++
@@ -1,8 +0,0 @@
-#ifndef TEMPSTRUCT_H
-#define TEMPSTRUCT_H
-
-#include <stdio.h>
-
-void print(int value);
-
-#endif // TEMPSTRUCT_H
