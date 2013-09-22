af8114c98ab0b7f29ec00039f317b390dd513eee
Alex Semin <alllex.semin@gmail.com>
2013-09-22 14:33:19 +0400

    new fib function

--- sem3/Haskell/hometask3.hs
+++ sem3/Haskell/hometask3.hs
@@ -3,14 +3,15 @@
 #2. Список всех чисел Фиббоначи
 -}
 
---pretty but slow
 p = 2:[x| x <- [3,5..], pr x] where
     pr x = foldr (\d acc -> if mod x d == 0 then False
                             else if d*d > x then True 
                                  else acc) True p
 
---cheat but short function uses filter
 p2 = 2 : f [3,5..] where f (x:xs) = x : (f $ filter (\y -> mod y x /= 0) xs)
 
 
-fibs = 0:1: (zipSum fibs $ tail fibs) where zipSum (x:xs) (y:ys) = x + y : zipSum xs ys
+fibs = 0 : 1 : (zipSum fibs $ tail fibs) 
+	where zipSum (x:xs) (y:ys) = x + y : zipSum xs ys
+
+fibs2 = fibs 0 1 where fibs a b = a : fibs b (a+b)
