62db6d9f0720531c9f08f4bc050160f028bd18e9
Alex Semin <Alllex.Semin@gmail.com>
2013-09-26 20:23:25 +0400

    Add Unit tests

--- sem3/FSharp/RegExpEMailCheck.fs
+++ sem3/FSharp/RegExpEMailCheck.fs
@@ -1,16 +1,22 @@
 open System.Text.RegularExpressions
+open NUnit.Framework
+open FsUnit
 
-let name = "^[a-zA-Z0-9~!#$:\-%\^*()]{0,30}"
-let fullName = name + "[.]" + name + "@"
-let domain2 = "(([a-zA-Z0-9\-~!#$:%\^*()]){1,30}\.)+"
+let letter = "a-zA-Z"
+let digits = "0-9"
+let specials = "\-!#$%^&\*\(\)/"
+let all = letter + digits + specials
+let fstChar = "[" + letter + digits + "]"
+
+let name = fstChar + "[" + all + "]{0,30}"
+let fullName = "^" + name + "[.]?" + name + "@"
+let domain2 = "([" + all + "]{1,30}\.)+"
 let domain1 = "(name|info|yandex|museum|[a-zA-Z]{2,3})$"
 
 let check = new Regex(fullName + domain2 + domain1)
 
 let correct = [ "alllex.semin@gmail.com"
-                "mE9ak!11er100500@mail.ru"
                 "superman323@supersite.info"
-                "support@yandex.ru"
                 "victor.polozov@gmail.com"
                 "victorp@math.spbu.ru"
                 "victor.polozov@lanit-tercom.com" ]
@@ -29,5 +35,25 @@ let incorrect = [ ".!!!!!!@gmail.com"
 let test1 = List.fold (fun acc mail -> acc && (check.IsMatch(mail))) true correct
 let test2 = List.fold (fun acc mail -> acc || (check.IsMatch(mail))) false incorrect
 
-printfn "%A" test1
-printfn "%A" test2
+
+[<Test>]
+let ``alllex.semin(at)gmail.com is correct eMail`` () =
+    (check.IsMatch("alllex.semin@gmail.com")) |> should equal true
+
+[<Test>]
+let ``superman323(at)supersite.info is correct eMail`` () =
+    (check.IsMatch("superman323@supersite.info")) |> should equal true
+
+[<Test>]
+let ``example(at)(at)mail.ru is NOT correct eMail`` () =
+    (check.IsMatch("example@@mail.ru")) |> should equal false
+
+[<Test>]
+let ``info(at)dom.c0m is NOT correct eMail`` () =
+    (check.IsMatch("info@dom.c0m")) |> should equal false
+
+[<Test>]
+let ``(at)gmail.com is NOT correct eMail`` () =
+    check.IsMatch("@gmail.com") |> should equal false
+
+
