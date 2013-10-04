18be41c6f86ea56b45239af9c9f57db5e2f80b85
Alex Semin <alllex.semin@gmail.com>
2013-10-05 00:26:50 +0400

    Corrected test tasks

--- sem3/Haskell/test1.hs
+++ sem3/Haskell/test1.hs
@@ -10,23 +10,26 @@ zipWith' _ _ _ = []
 
 sprod x y = sum $ zipWith' (*) x y
 
-normilize l = foldr (\x acc -> if elem x acc then acc else x:acc) [] l
+normalize l = foldr (\x acc -> if elem x acc then acc else x:acc) [] l
 
 qsort [] = []
 qsort (x:xs) = qsort [a | a <- xs, a <= x] ++ [x] ++ qsort [a | a <- xs, a > x]
 
-isProgress [] = True
-isProgress [x] = True
-isProgress xs = 1 == (length $ normilize (zipWith' (-) xs $ tail xs))
+isProgression (x:t@(y:ys)) = check t where
+    d = y - x
+    check (x:t@(y:ys)) = y - x == d && check t
+    check _ = True
+isProgression _ = True
 
 isFunction [] = True
-isFunction (r:rs) = (length [1 | (x,_) <- rs, (fst r) == x]) == 0 && isFunction rs
+isFunction (r:rs) = [1 | (x,_) <- rs, (fst r) == x] == [] && isFunction rs
 
 isSymmetric r = foldr (&&) True $ map (\(x,y) -> elem (y,x) r) r
 
 isReflexive r = foldr (\x acc -> if (elem (x,x) r) && elem (-x,-x) r then acc else False) True [0,1..]
+isReflexive' r = length r == -1
 
 closure r = if cl == [] then r else closure $ r ++ cl
-    where cl = normilize [(x,z) | a@(x,y1) <- r, b@(y2,z) <- r, y1 == y2, a /= b, not $ elem (x,z) r]
+    where cl = normalize [(x,z) | a@(x,y1) <- r, b@(y2,z) <- r, y1 == y2, a /= b, not $ elem (x,z) r]
 
-isTransitive r = foldr (&&) True [elem (x,z) r | a@(x,y1) <- r, b@(y2,z) <- r, y1 == y2, a /= b]
+isTransitive r = length r == (length $ closure r)
