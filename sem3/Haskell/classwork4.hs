29e6525aad072d1c6b0d0bb6ae1dbb4808bff2ea
Alex Semin <alllex.semin@gmail.com>
2013-10-04 23:39:48 +0400

    ..

--- sem3/Haskell/classwork4.hs
+++ sem3/Haskell/classwork4.hs
@@ -1,36 +1,48 @@
-{- 
-  Hometask #4
-  Binary Trees
--}
+
+
+data Color = Red|Green|Blue
+
+colors = [Red, Green, Blue]
+
+colorsString x = map (\s -> case s of
+                              Red   -> "Red"
+                              Green -> "Green"
+                              Blue  -> "Blue"
+                     ) x                               
+                     
+data Sound = Sound Int Int
+
+sounds = map (Sound 1) [3,4,5,6] 
+inRange (Sound _ j) = j >= 20 && j <= 2000 
+
+data L a = Cons a (L a) | Nil
+-- data List a = a ':' (List a) | []
+
+len Nil = 0
+len (Cons _ l) = len l + 1
 
 data Tree a = E | T (Tree a) a (Tree a)
 
-nnodes :: Tree Integer -> Integer
 nnodes E = 0
 nnodes (T l _ r) = nnodes l + nnodes r + 1
 
-nleafs :: Tree Integer -> Integer
 nleafs E = 0
 nleafs (T E _ E) = 1
 nleafs (T l _ r) = nleafs l + nleafs r
 
-depth :: Tree Integer -> Integer
 depth E = 0
 depth (T l _ r) = 1 + max (depth l) (depth r)
 
-leafes :: Tree Integer -> [Integer]
 leafes = leafes' [] where
   leafes' acc E = acc
   leafes' acc (T E x E) = x:acc
   leafes' acc (T l x r) = leafes' (leafes' acc l) r
 
-maxn :: Tree Integer -> Integer
 maxn E = error "!!!"
 maxn (T l x r) = maxn' (maxn' x l) r where
   maxn' x E = x
   maxn' x (T l y r) = maxn' (maxn' (max x y) l) r  
   
-factor :: Integer -> Tree Integer -> (Integer, Integer)
 factor n E = (0,0)
 factor n (T l x r) = (factor n l !+ factor n r) !+ if x < n then (1,0) else (0,1) where
   (a,b) !+ (c,d) = (a + c,b + d)
@@ -54,13 +66,16 @@ lf :: Integer -> Tree Integer
 lf x = T E x E
 
 isTree :: Tree Integer -> Bool
-isTree t = isT t Nothing Nothing where 
-    isT E _ _ = True
-    isT (T l x r) mn mx = let jx = Just x in 
-        mn < jx && (mx == Nothing || jx < mx) && isT l mn jx && isT r jx mx
+isTree t = isT t Nothing Nothing 
+    where isT E _ _ = True
+          isT (T l x r) min' max' = 
+            x !> min' && x <! max' && isT l min' (Just x) && isT r (Just x) max'
+            where x !> Nothing = True
+                  x !> (Just y) = x > y
+                  x <! Nothing = True
+                  x <! (Just y) = x < y
              
 
 elements :: Tree Integer -> [Integer]
-elements t = elems t [] where
-    elems (T l x r) acc = elems l $ x:elems r acc
-    elems _ acc = acc
+elements E = []
+elements (T l x r) = (elements l) ++ [x] ++ (elements r)
