04006c5872fb3ee2e24d051480cb173e3196fbf8
Alex Semin <Alllex.Semin@gmail.com>
2013-10-10 23:04:01 +0400

    Initial commit

---
+++ sem3/FSharp/MailCheck/MailCheck/Program.fs
@@ -0,0 +1,28 @@
+﻿(*
+    Project: MailCheck
+    Author: Alex Semin Math-Mech 271 2013
+    2013 (c)
+*)
+
+module MailCheck
+
+open System.Text.RegularExpressions
+
+type EMailChecker() =
+    class
+        static let letters = "a-zA-Z"
+        static let digits = "0-9"
+        static let specials = "_\-!#$%^&)(/"
+        static let all = letters + digits + specials
+        static let fstChar = "[_" + letters + "]"
+
+        static let name = "[" + all + "]{0,30}"
+        static let fullName = "^" + fstChar + name + "[.]?" + name + "@"
+        static let domain2 = "([" + all + "]{1,30}\.)+"
+        static let domain1 = "(name|info|yandex|museum|[a-zA-Z]{2,3})$"
+
+        static let emailChecker = new Regex(fullName + domain2 + domain1)
+
+        member this.Check(email : string) = emailChecker.IsMatch(email)
+    end
+
---
+++ sem3/FSharp/MailCheck/Tests/Program.fs
@@ -0,0 +1,23 @@
+﻿
+open MailCheck
+open FsUnit
+open NUnit.Framework
+
+[<TestFixture>]
+type MailCheckFixture() =
+
+    let correct = [ "a@b.cc"
+                    "victor.polozov@mail.ru"
+                    "my@domain.info"
+                    "_.1@mail.com"
+                    "coins_department@hermitage.museum" ]
+              
+    let incorrect = [ "a@b.c"
+                      "a..b@mail.ru"
+                      ".a@mail.ru"
+                      "yo@domain.somedomain"
+                      "1@mail.ru" ]
+
+    [<Test>]
+    member x.``Email: a(at)b.cc`` () =
+        (new EMailChecker()).Check("a@b.cc") |> should be True
