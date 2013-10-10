d10e4baf71afbe9425b9e5eb3e34a4bbf6db769c
Alex Semin <Alllex.Semin@gmail.com>
2013-10-10 23:45:17 +0400

    Tests on lists

--- sem3/FSharp/MailCheck/Tests/Program.fs
+++ sem3/FSharp/MailCheck/Tests/Program.fs
@@ -18,6 +18,16 @@ type MailCheckFixture() =
                       "yo@domain.somedomain"
                       "1@mail.ru" ]
 
+    let checker = new EMailChecker()
+
     [<Test>]
-    member x.``Email: a(at)b.cc`` () =
-        (new EMailChecker()).Check("a@b.cc") |> should be True
+    member x.``Email: alllex.semin(at)gmail.com`` () =
+        checker.Check("alllex.semin@gmail.com") |> should be True
+
+    [<Test>]
+    member x.AllCorrect() =
+        correct |> List.iter (fun email -> checker.Check(email) |> should be True)
+        
+    [<Test>]
+    member x.AllIncorrect() =
+        incorrect |> List.iter (fun email -> checker.Check(email) |> should be False)
