c09778e80b7a4ae4a5aff91a46a61b9b64976540
Alex Semin <Alllex.Semin@gmail.com>
2013-10-13 17:58:06 +0400

    Getting sublinks

--- sem3/FSharp/WebCrawler/WebCrawler/Program.fs
+++ sem3/FSharp/WebCrawler/WebCrawler/Program.fs
@@ -1,7 +1,34 @@
-﻿// Learn more about F# at http://fsharp.net
-// See the 'F# Tutorial' project for more help.
+﻿open System.Net
+open Microsoft.FSharp.Control.WebExtensions
+open System.IO
+open System.Text.RegularExpressions
 
-[<EntryPoint>]
-let main argv = 
-    printfn "%A" argv
-    0 // return an integer exit code
+
+let url' = "http://en.wikipedia.org/wiki/Bit"
+let uri = new System.Uri(url')
+let webClient = new WebClient()
+let html = webClient.DownloadString(url')
+
+let link = new Regex("href\s*=\s*\"(?<addr>.+?\/?)\"")
+
+let links = [for m in link.Matches(html) -> m.Result("${addr}")] //|> List.iter (printfn "%s")
+
+let process_link (link : string) = 
+    if link.StartsWith("http") then link
+    else if link.StartsWith("/") then url' + link.Substring(1)
+         else ""
+
+let check_site (link : string) =
+    try 
+        webClient.DownloadString(link) |> ignore
+        true
+    with
+    | ex -> false
+
+links 
+    |> List.map process_link
+    |> List.filter (fun l -> l <> "")
+    |> List.filter check_site
+    |> List.iter (fun l -> printfn "[%b] %s" (check_site l) l)
+
+
