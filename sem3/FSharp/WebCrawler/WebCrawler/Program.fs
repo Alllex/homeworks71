411f8cec21a1440c035f99baa26ec3e9e65f4fca
Alex Semin <Alllex.Semin@gmail.com>
2013-10-16 00:09:06 +0400

    ..

--- sem3/FSharp/WebCrawler/WebCrawler/Program.fs
+++ sem3/FSharp/WebCrawler/WebCrawler/Program.fs
@@ -4,31 +4,98 @@ open System.IO
 open System.Text.RegularExpressions
 
 
-let url' = "http://en.wikipedia.org/wiki/Bit"
-let uri = new System.Uri(url')
-let webClient = new WebClient()
-let html = webClient.DownloadString(url')
+let rmDup list = List.foldBack (fun x acc -> if List.tryFind ((=) x) acc <> None then acc else x::acc) list []
 
-let link = new Regex("href\s*=\s*\"(?<addr>.+?\/?)\"")
+let linkRegex = new Regex("\"(?<addr>[^\"\?=]+)(\"|\?|=)+?")
+let picRegex = Regex("(\"|\')(?<pic>[^\"\?=]+\.(png|jpe?g))(\'|\"|\?|=)+?")
 
-let links = [for m in link.Matches(html) -> m.Result("${addr}")] //|> List.iter (printfn "%s")
+let tillLastSlash (link : string) =
+    link.Substring(0,link.LastIndexOf('/') + 1)
 
-let process_link (link : string) = 
+let processLink (dom : string) (link : string) = 
     if link.StartsWith("http") then link
-    else if link.StartsWith("/") then url' + link.Substring(1)
-         else ""
+    else if link.StartsWith("//") then "http:" + link
+         else if dom.EndsWith("/") then
+                if link.StartsWith("/") then dom + link.Substring(1) else dom + link
+              else tillLastSlash dom + link
 
-let check_site (link : string) =
-    try 
-        webClient.DownloadString(link) |> ignore
-        true
+let folder = "pics"                         
+if not <| Directory.Exists(folder) then Directory.CreateDirectory(folder) |> ignore
+
+let localName name = 
+    let rightChar c = (List.tryFind ((=) c) ['/';'\\';':';'?';'\"';'*';'<';'>']) <> None
+    folder + "\\" +  (String.map (fun c -> if rightChar c then '_' else c) name)
+
+let download link =
+  async { 
+    try
+        let wc = new WebClient()
+        wc.DownloadFileAsync(new System.Uri(link), localName link) 
+    with | ex -> () //printfn "Download stoped: pic[%s] msg:%s" link (ex.Message)
+  }
+
+
+let maxSize = 1024000L
+let minSize = 200000L
+
+let sizePredicate (url : string) = 
+    let wr = WebRequest.Create(url)
+    let wres = wr.GetResponse()
+    let size = wres.ContentLength
+    wres.Close()
+    size > minSize && size < maxSize
+
+let downloadPicsTask (link : string) =
+  async {
+    try  
+        let webClient = new WebClient()
+        let! html = webClient.AsyncDownloadString(new System.Uri(link))
+        let pics = [for m in picRegex.Matches(html) -> m.Result("${pic}")]
+                    |> List.map (processLink link)
+                    |> List.filter ((<>) "")
+                    |> List.filter (fun (s:string) -> s.IndexOf("1366") > 0)
+                    |> rmDup
+                    |> List.filter sizePredicate
+                    |> Seq.map download
+                    |> Async.Parallel 
+                    |> Async.RunSynchronously
+                    |> ignore
+        ()
+        printfn "END: Site [%s] processed" <| link.Substring(0,70)
     with
-    | ex -> false
+        | ex -> ()// printfn "Downloading pics problem: site[%s] msg:%s" link (ex.Message)
+  }
 
-links 
-    |> List.map process_link
-    |> List.filter (fun l -> l <> "")
-    |> List.filter check_site
-    |> List.iter (fun l -> printfn "[%b] %s" (check_site l) l)
+  
+let linkRegex' = new Regex("\"(?<addr>[^\s\"]+?)\"") 
+
+let findLinks' link =  
+    try 
+        let webClient = new WebClient()
+        let html = webClient.DownloadString(new System.Uri(link))
+        let links = [for m in linkRegex'.Matches(html) -> m.Result("${addr}")]
+                        |> List.map (processLink link)
+                        |> List.filter ((<>) "")
+                        |> rmDup
+                        |> List.filter (fun (s:string) -> s.IndexOf("hdwallpapers") > 0)
+        links
+    with 
+        | ex -> []//printfn "Finding links problem: site[%s] msg:%s" link (ex.Message); []
 
 
+let startUrl = "http://www.hdwallpapers.in/"
+
+let mutable checked' = []
+
+let rec crawlTheNet (deepth : int) (url : string) =
+    let links' = url :: findLinks' url |> List.filter (fun s -> List.tryFind ((=) s) checked' = None)
+    checked' <- links' @ checked'
+    links'
+        |> Seq.map downloadPicsTask
+        |> Async.Parallel 
+        |> Async.RunSynchronously
+        |> ignore//*)
+    if deepth > 0 then 
+        List.map (crawlTheNet (deepth - 1)) links' |> ignore
+
+crawlTheNet 5 startUrl
