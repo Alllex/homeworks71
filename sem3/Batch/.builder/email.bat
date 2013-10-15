79d53ef3bd88f873b776645c245a3157cda76d1e
Alex Semin <Alllex.Semin@gmail.com>
2013-10-15 11:38:51 +0400

    Say Yes! for DRY programs

--- sem3/Batch/.builder/email.bat
+++ sem3/Batch/.builder/email.bat
@@ -4,11 +4,8 @@ if %builder_start%=="" goto :EOF
 
 echo Sending email with building result...
 
-if not exist %email_attached_files% (
-    blat -body " " -tf %email_list% -server %email_server% -f %email_from% -subject "%email_subject% [%email_extra_subject%]" >%email_log%
-) else (
-    blat -body " " -tf %email_list% -server %email_server% -f %email_from% -subject "%email_subject% [%email_extra_subject%]" -atf %email_attached_files% >%email_log%
-)
+if not exist %email_attached_files% echo. >%email_attached_files%
+blat -body " " -tf %email_list% -server %email_server% -f %email_from% -subject "%email_subject% [%email_extra_subject%]" -atf %email_attached_files% >%email_log%
 
 echo Email(s) has been sent
 
