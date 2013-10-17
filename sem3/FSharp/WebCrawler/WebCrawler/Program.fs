4f07d2d05a47cd85abf6c442a8546ede4eab9b59
Alex Semin <Alllex.Semin@gmail.com>
2013-10-18 02:55:17 +0400

    Many changes, nothing works

--- sem3/FSharp/WebCrawler/WebCrawler/Program.fs
+++ sem3/FSharp/WebCrawler/WebCrawler/Program.fs
@@ -3,10 +3,11 @@ open Microsoft.FSharp.Control.WebExtensions
 open System.IO
 open System.Text.RegularExpressions
 
+let printAll = List.iter (printfn "[%s]")
 
 let rmDup list = List.foldBack (fun x acc -> if List.tryFind ((=) x) acc <> None then acc else x::acc) list []
 
-let linkRegex = new Regex("\"(?<addr>[^\"\?=]+)(\"|\?|=)+?")
+let linkRegex = new Regex("\"(?<addr>[^\s\"]+?)\"") 
 let picRegex = Regex("(\"|\')(?<pic>[^\"\?=]+\.(png|jpe?g))(\'|\"|\?|=)+?")
 
 let tillLastSlash (link : string) =
@@ -30,72 +31,106 @@ let download link =
   async { 
     try
         let wc = new WebClient()
-        wc.DownloadFileAsync(new System.Uri(link), localName link) 
-    with | ex -> () //printfn "Download stoped: pic[%s] msg:%s" link (ex.Message)
+        wc.DownloadFile(new System.Uri(link), localName link) 
+    with | ex -> printfn "%s" ex.Message
   }
 
-
-let maxSize = 1024000L
-let minSize = 200000L
+let maxSize = 5000L * 1024L
+let minSize = 500L * 1024L
 
 let sizePredicate (url : string) = 
+  try
     let wr = WebRequest.Create(url)
     let wres = wr.GetResponse()
     let size = wres.ContentLength
     wres.Close()
     size > minSize && size < maxSize
+  with | ex -> false
 
+(*
 let downloadPicsTask (link : string) =
   async {
     try  
+        printfn "Downloading from [%s]" link
         let webClient = new WebClient()
         let! html = webClient.AsyncDownloadString(new System.Uri(link))
         let pics = [for m in picRegex.Matches(html) -> m.Result("${pic}")]
                     |> List.map (processLink link)
-                    |> List.filter ((<>) "")
-                    |> List.filter (fun (s:string) -> s.IndexOf("1366") > 0)
                     |> rmDup
+        //printAll pics
+        pics
                     |> List.filter sizePredicate
                     |> Seq.map download
                     |> Async.Parallel 
                     |> Async.RunSynchronously
                     |> ignore
         ()
-        printfn "END: Site [%s] processed" <| link.Substring(0,70)
     with
-        | ex -> ()// printfn "Downloading pics problem: site[%s] msg:%s" link (ex.Message)
+        | ex -> ()
   }
+*)
 
-  
-let linkRegex' = new Regex("\"(?<addr>[^\s\"]+?)\"") 
+let denieded = [".css"; ".js"; ".ico"; ".jpg"; ".png"]
 
-let findLinks' link =  
+let filterSome = List.filter (fun (l:string) -> List.fold (&&) true <| List.map (fun d -> not (l.Contains(d))) denieded)
+
+let onlyDeeper (dom : string) (link : string) =
+  try
+    let a = dom.IndexOf('/') + 2
+    let b = dom.Substring(a).IndexOf('/') + a
+    link.StartsWith(dom.Substring(0, b))
+  with | ex -> false
+
+let findLinks link =  
     try 
         let webClient = new WebClient()
         let html = webClient.DownloadString(new System.Uri(link))
-        let links = [for m in linkRegex'.Matches(html) -> m.Result("${addr}")]
+
+        let links = [for m in linkRegex.Matches(html) -> m.Result("${addr}")]
                         |> List.map (processLink link)
-                        |> List.filter ((<>) "")
-                        |> rmDup
-                        |> List.filter (fun (s:string) -> s.IndexOf("hdwallpapers") > 0)
-        links
+                        //|> List.filter (onlyDeeper link)
+                        |> List.filter (fun (l:string) -> l.StartsWith(link))
+                        |> List.filter ((<>) link)
+                        |> filterSome
+
+        let pics = [for m in picRegex.Matches(html) -> m.Result("${pic}")]
+                        |> List.map (processLink link)
+
+        (links, pics)
     with 
-        | ex -> []//printfn "Finding links problem: site[%s] msg:%s" link (ex.Message); []
+        | ex -> ([],[])
 
 
-let startUrl = "http://www.hdwallpapers.in/"
+let startUrl = "http://imgsrc.hubblesite.org/"
+let startUrl' = "http://photography.nationalgeographic.com/photography/photos/extreme-earth/"
 
-let mutable checked' = []
+//let checked' = new ResizeArray<_>()
 
-let rec crawlTheNet (deepth : int) (url : string) =
-    let links' = url :: findLinks' url |> List.filter (fun s -> List.tryFind ((=) s) checked' = None)
-    checked' <- links' @ checked'
-    links'
-        |> Seq.map downloadPicsTask
+let rec picsLinks (url:string) = 
+  async {
+    printfn "New link:[%s]" url
+    //checked'.Add(url)
+    let (links, pics) = findLinks url
+    let ls = links |> rmDup //|> List.filter (fun l -> not <| checked'.Contains l)
+    pics
+        |> rmDup 
+        |> List.filter sizePredicate
+        |> Seq.map download
         |> Async.Parallel 
-        |> Async.RunSynchronously
-        |> ignore//*)
-    if deepth > 0 then 
-        List.map (crawlTheNet (deepth - 1)) links' |> ignore
+        |> Async.StartAsTask
+        |> ignore
 
-crawlTheNet 5 startUrl
+    ls  |> Seq.map picsLinks
+        |> Async.Parallel 
+        |> Async.StartAsTask
+        |> System.Threading.Tasks.Task.WaitAll
+  }
+
+let picsList = picsLinks startUrl'
+
+Async.RunSynchronously picsList
+
+(*
+printfn "Pic list:"
+printAll picsList*)
+printfn "Start waiting..."
