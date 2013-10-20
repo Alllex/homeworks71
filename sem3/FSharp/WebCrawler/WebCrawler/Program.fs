e260e462c1d08d679cd3d313018f171b21302739
Alex Semin <Alllex.Semin@gmail.com>
2013-10-20 20:44:32 +0400

    Create class and GUI

--- sem3/FSharp/WebCrawler/WebCrawler/Program.fs
+++ sem3/FSharp/WebCrawler/WebCrawler/Program.fs
@@ -1,136 +1,157 @@
-﻿open System.Net
+﻿
+module WebCrawler
+
+open System.Net
 open Microsoft.FSharp.Control.WebExtensions
 open System.IO
 open System.Text.RegularExpressions
+open System.Collections
 
-let printAll = List.iter (printfn "[%s]")
+type List<'T> with
+    static member removeDuplicates xs =
+        List.foldBack (fun x acc -> if List.tryFind ((=) x) acc <> None then acc else x::acc) xs []
+    end
 
-let rmDup list = List.foldBack (fun x acc -> if List.tryFind ((=) x) acc <> None then acc else x::acc) list []
+type System.String with
+    member s.tillLastChar(c : char) = s.Substring(0, s.LastIndexOf('/') + 1)
+    member s.toWinObjName = 
+        let rightChar c = (List.tryFind ((=) c) ['/';'\\';':';'?';'\"';'*';'<';'>']) <> None
+        (String.map (fun c -> if rightChar c then '_' else c) s)
 
-let linkRegex = new Regex("\"(?<addr>[^\s\"]+?)\"") 
-let picRegex = Regex("(\"|\')(?<pic>[^\"\?=]+\.(png|jpe?g))(\'|\"|\?|=)+?")
+type WebCrawlerStates = Ready | Working
 
-let tillLastSlash (link : string) =
-    link.Substring(0,link.LastIndexOf('/') + 1)
+type WebCrawler(url : string, minSizePic, maxSizePic: int64) =
+    class
 
-let processLink (dom : string) (link : string) = 
-    if link.StartsWith("http") then link
-    else if link.StartsWith("//") then "http:" + link
-         else if dom.EndsWith("/") then
-                if link.StartsWith("/") then dom + link.Substring(1) else dom + link
-              else tillLastSlash dom + link
+        static let urlRegex = new Regex("\"(?<addr>[^\s\"]+?)\"") 
+        static let picRegex = new Regex("(\"|\')(?<pic>[^\"\?=]+\.(png|jpe?g|gif))(\'|\"|\?|=)+?")
 
-let folder = "pics"                         
-if not <| Directory.Exists(folder) then Directory.CreateDirectory(folder) |> ignore
+        static let maxSizeDefault = 3L * 1024L * 1024L // 3 Mb
+        static let minSizeDefault = 200L * 1024L // 10 Kb
+        static let depthLimit = 10
+        
+        static let deniedInSiteUrl = [".css"; ".js"; ".ico"; ".jpg"; ".png"; ".gif"]
+        
+        static let filterDenied (siteUrl : string) = 
+            List.fold (&&) true <| List.map (fun d -> not (siteUrl.Contains(d))) deniedInSiteUrl
 
-let localName name = 
-    let rightChar c = (List.tryFind ((=) c) ['/';'\\';':';'?';'\"';'*';'<';'>']) <> None
-    folder + "\\" +  (String.map (fun c -> if rightChar c then '_' else c) name)
+        static let isSubUrl (dom : string) (url : string) =
+            (*try
+                let a = dom.IndexOf('/') + 2
+                let b = dom.Substring(a).IndexOf('/') + a
+                url.StartsWith(dom.Substring(0, b))
+            with | ex -> false*)
+            url.StartsWith(dom)
 
-let download link =
-  async { 
-    try
-        let wc = new WebClient()
-        wc.DownloadFile(new System.Uri(link), localName link) 
-    with | ex -> printfn "%s" ex.Message
-  }
+        static let buildUrl (dom : string) (url : string) = 
+            if url.StartsWith("http") then url
+            else if url.StartsWith("//") then "http:" + url
+                 else if dom.EndsWith("/") then if url.StartsWith("/") then dom + url.Substring(1) 
+                                                else dom + url
+                      else dom.tillLastChar('/') + url
 
-let maxSize = 5000L * 1024L
-let minSize = 500L * 1024L
+// -----------------------------------------------------------------------------------------
 
-let sizePredicate (url : string) = 
-  try
-    let wr = WebRequest.Create(url)
-    let wres = wr.GetResponse()
-    let size = wres.ContentLength
-    wres.Close()
-    size > minSize && size < maxSize
-  with | ex -> false
+        let startUrl = url
+        let folderName = url.toWinObjName
+        
+        let EUrlSearchCompleted = new Event<_>()
+        let EFilesDownloadCompleted = new Event<_>()
+        let EFileDownloadCompleted = new Event<_>()
+        let ELogMessage = new Event<_>()
 
-(*
-let downloadPicsTask (link : string) =
-  async {
-    try  
-        printfn "Downloading from [%s]" link
-        let webClient = new WebClient()
-        let! html = webClient.AsyncDownloadString(new System.Uri(link))
-        let pics = [for m in picRegex.Matches(html) -> m.Result("${pic}")]
-                    |> List.map (processLink link)
-                    |> rmDup
-        //printAll pics
-        pics
-                    |> List.filter sizePredicate
-                    |> Seq.map download
-                    |> Async.Parallel 
-                    |> Async.RunSynchronously
-                    |> ignore
-        ()
-    with
-        | ex -> ()
-  }
-*)
+        let minSize = minSizePic
+        let maxSize = maxSizePic
 
-let denieded = [".css"; ".js"; ".ico"; ".jpg"; ".png"]
+        let mutable state = Ready
+        
+        let checkedPages = ArrayList.Synchronized (new ArrayList())
 
-let filterSome = List.filter (fun (l:string) -> List.fold (&&) true <| List.map (fun d -> not (l.Contains(d))) denieded)
+        let sizePredicate (url : string) = 
+            try
+                let wreq = WebRequest.Create(url)
+                let wres = wreq.GetResponse()
+                let size = wres.ContentLength
+                wres.Close()
+                size > minSize && size < maxSize
+            with | ex -> false
+            
+        let downloadPic (fileUrl : string) = 
+          async {
+            try
+                ELogMessage.Trigger("\r\nStart download: " + fileUrl)
+                let wc = new WebClient()
+                wc.DownloadFileCompleted.Add(fun e -> (*
+                    if not (e.Cancelled || e.Error = null) then **)EFileDownloadCompleted.Trigger("\r\nDownloaded: " + fileUrl))
+                wc.DownloadFile(new System.Uri(fileUrl), folderName + "\\" + fileUrl.toWinObjName) 
+            with | ex -> ()
+          }
 
-let onlyDeeper (dom : string) (link : string) =
-  try
-    let a = dom.IndexOf('/') + 2
-    let b = dom.Substring(a).IndexOf('/') + a
-    link.StartsWith(dom.Substring(0, b))
-  with | ex -> false
+        let findUrls (url : string) = 
+            try 
+                let webClient = new WebClient()
+                let html = webClient.DownloadString(new System.Uri(url))
+                seq { for m in urlRegex.Matches(html) -> m.Result("${addr}") }
+                    |> Seq.map (buildUrl url)
+                    |> Seq.filter ((<>) url)
+                    |> Seq.filter (isSubUrl url)
+                    |> Seq.filter filterDenied
+            with | ex -> Seq.empty
 
-let findLinks link =  
-    try 
-        let webClient = new WebClient()
-        let html = webClient.DownloadString(new System.Uri(link))
+        let downloadPics (url : string) = 
+            async {
+                try
+                    let webClient = new WebClient()
+                    let html = webClient.DownloadString(new System.Uri(url))
+                    ELogMessage.Trigger("\r\nSearch for pics on: " + url)
+                    seq { for m in picRegex.Matches(html) -> m.Result("${pic}") }
+                        |> Seq.map (buildUrl url)
+                        |> Seq.map downloadPic
+                        |> Async.Parallel
+                        |> Async.RunSynchronously
+                        |> ignore
+                with | ex -> ()
+            }
 
-        let links = [for m in linkRegex.Matches(html) -> m.Result("${addr}")]
-                        |> List.map (processLink link)
-                        //|> List.filter (onlyDeeper link)
-                        |> List.filter (fun (l:string) -> l.StartsWith(link))
-                        |> List.filter ((<>) link)
-                        |> filterSome
+        let rec crawl (url : string) = 
+            async {
+                try
+                    if not(checkedPages.Contains(url)) then 
+                        ELogMessage.Trigger("\r\nCrawl site: " + url)
+                        Async.Start (downloadPics url)
+                        checkedPages.Add(url) |> ignore
+                        findUrls url
+                            |> Seq.map crawl
+                            |> Async.Parallel
+                            |> Async.RunSynchronously
+                            |> ignore
+                with | ex -> ()
+            }
+            
 
-        let pics = [for m in picRegex.Matches(html) -> m.Result("${pic}")]
-                        |> List.map (processLink link)
 
-        (links, pics)
-    with 
-        | ex -> ([],[])
+// -----------------------------------------------------------------------------------------
 
+        do
+            if not <| Directory.Exists(folderName) then Directory.CreateDirectory(folderName) |> ignore 
+             
+             
+// -----------------------------------------------------------------------------------------
 
-let startUrl = "http://imgsrc.hubblesite.org/"
-let startUrl' = "http://photography.nationalgeographic.com/photography/photos/extreme-earth/"
-
-//let checked' = new ResizeArray<_>()
-
-let rec picsLinks (url:string) = 
-  async {
-    printfn "New link:[%s]" url
-    //checked'.Add(url)
-    let (links, pics) = findLinks url
-    let ls = links |> rmDup //|> List.filter (fun l -> not <| checked'.Contains l)
-    pics
-        |> rmDup 
-        |> List.filter sizePredicate
-        |> Seq.map download
-        |> Async.Parallel 
-        |> Async.StartAsTask
-        |> ignore
-
-    ls  |> Seq.map picsLinks
-        |> Async.Parallel 
-        |> Async.StartAsTask
-        |> System.Threading.Tasks.Task.WaitAll
-  }
-
-let picsList = picsLinks startUrl'
-
-Async.RunSynchronously picsList
-
-(*
-printfn "Pic list:"
-printAll picsList*)
-printfn "Start waiting..."
+        new(url : string) = WebCrawler(url, minSizeDefault, maxSizeDefault)    
+                
+        member public this.Start() =
+            match state with
+            | Ready -> Async.Start (crawl startUrl); state <- Working; "Started"
+            | _ -> "Already working"
+            
+            
+        [<CLIEvent>]
+        member public this.UrlSearchCompleted = EUrlSearchCompleted.Publish
+        [<CLIEvent>]
+        member public this.FileDownloadCompleted = EFileDownloadCompleted.Publish
+        [<CLIEvent>]
+        member public this.FilesDownloadCompleted = EFilesDownloadCompleted.Publish
+        [<CLIEvent>]
+        member public this.LogMessage = ELogMessage.Publish
+    end
---
+++ sem3/FSharp/WebCrawler/WebCrawlerGUI/Program.fs
@@ -0,0 +1,39 @@
+﻿
+open System
+open System.Windows.Forms
+open System.Drawing
+open WebCrawler
+
+type WebCrawlerForm(w:int,h:int) as this = 
+    class
+        inherit Form(Text="WebCrawler: ",Width=w,Height=h)
+        let logHeight = 400
+        let tbStartUrl = new TextBox(Text="",Left=0,Top=5,Width=300)
+        let tbLog = new TextBox(
+                                    Multiline=true,
+                                    ScrollBars=ScrollBars.Vertical,
+                                    Text="",
+                                    Left=0,
+                                    Top=h-logHeight-50,
+                                    Width=w-50,
+                                    Height=logHeight,
+                                    Visible=true,
+                                    Enabled=true,
+                                    ReadOnly=true
+                               )
+        let bStart = new Button(Text="Start",Top=5,Left=500)
+        do
+            this.Controls.Add(tbLog)
+            this.Controls.Add(bStart)
+            this.Controls.Add(tbStartUrl)
+            bStart.Click.Add(fun _ -> 
+                let wc = new WebCrawler(tbStartUrl.Text)
+                wc.LogMessage.Add(tbLog.AppendText)
+                wc.FileDownloadCompleted.Add(tbLog.AppendText)
+                this.Text <- wc.Start())
+            
+    end
+
+let form = new WebCrawlerForm(700,500)
+
+Application.Run(form)
