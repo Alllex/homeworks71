b7e2aa7def82bfab282b29e5f91f8cba54c15255
Alex Semin <Alllex.Semin@gmail.com>
2014-03-12 18:51:43 +0400

    Rewrite parsers with seq

--- sem4/ParserCombinator/Core.fs
+++ sem4/ParserCombinator/Core.fs
@@ -1,35 +1,146 @@
 ﻿
 module ParserCombinators.Core
 
+open System
+
 let chars2str cs = string (new System.String (cs |> List.toArray))
 let chars2int s = int (new System.String(s |> List.toArray))
+let str2chars (s : string) = List.ofArray <| s.ToCharArray()     
 
-type ParserResult<'a> =
-    | Success of 'a * list<char> 
-    | Failure of char list
+type ParserInfo(str : char list, line : int, col : int, expected : string) =
+    class
+        (*
+        new(str : char list, line : int, col : int, expected : string) = new ParserInfo(str, line, col, expected, "")
+        *)
+        new(pi : ParserInfo, expected : string) = new ParserInfo(pi.Rest, pi.Line, pi.Column, expected)
+        member x.Rest = str
+        member x.Line = line
+        member x.Column = col
+        member x.ExpectedValue = expected
+        member x.IsSucceeded() = str.Length = 0 
+        (*
+        member x.HasError() = Option.isSome err
+        member x.IsStopped() = str.Length > 0 && not (x.HasError())
+        member x.ErrorMessage = if x.HasError() then err.Value.ErrorMessage else ""
+        *)
+        interface IComparable with
+            member x.CompareTo(o : obj) =
+                match o with
+                | :? ParserInfo as y -> str.Length.CompareTo(y.Rest.Length)
+                | _ -> -1
+        override x.Equals(o : obj) = 
+            match o with
+            | :? ParserInfo as y -> line = y.Line && col = y.Column && str = y.Rest
+            | _ -> false
+        override x.GetHashCode() = str.GetHashCode()
+        override x.ToString() =
+            let cut (len : int) (s : string) =
+                if s.Length > len then s.Substring(0, len)
+                else s
+            let s = sprintf "Parser stopped at line %d, column %d, where \"%s...\"" line col (cut 10 <| chars2str str)
+                    //+ if not (x.HasError()) then "" else "With error:\n" + x.ErrorMessage
+            s
 
-type Parser<'a> = list<char> -> ParserResult<'a>
+        static member StartInfo(s : string) = new ParserInfo(str2chars s, 0, 0, "")
+        //static member UnknownErrorInfo = new ParserInfo([], 0, 0, Some <| ParserError.UnknownParserError)
+    end
 
-/// Bind operator. Applies f to the result of parser p.
-let (>>=) (p : Parser<'a>) (f : 'a -> Parser<'b>) : Parser<'b> =
-    fun s ->
-        match p s with
-        | Success(x, rest) -> (f x) rest
-        | Failure cs -> Failure cs
+type ParserError(line : int, col : int, expected : string, errmsg : string) =
+    class
+        let exp = sprintf "Error: At line %d, column %d expected \"%s\"" line col expected
+        let error = if errmsg.Length = 0 then exp else errmsg
+        new(line : int, col : int, expected : string) = new ParserError(line, col, expected, "")
+        new(pi : ParserInfo, expected : string, errmsg : string) = new ParserError(pi.Line, pi.Column, expected, errmsg)
+        new(pi : ParserInfo, errmsg : string) = new ParserError(pi.Line, pi.Column, pi.ExpectedValue, errmsg)
+        member x.Line = line
+        member x.Column = col
+        member x.ExpectedValue = expected
+        member x.ErrorMessage = error
+        static member SymfParserErrorMsg = "Character doesn't match the function"
+        static member UnknownParserError = new ParserError(0, 0, "", "Unknown parser error")
+
+        interface IComparable with
+            member x.CompareTo(o : obj) =
+                match o with
+                | :? ParserError as y -> if line > y.Line then -1 else col.CompareTo(y.Column) 
+                | _ -> -1
+        override x.Equals(o : obj) = 
+            match o with
+            | :? ParserError as y -> line = y.Line && col = y.Column
+            | _ -> false
+        override x.GetHashCode() = exp.GetHashCode()
+        
+        override x.ToString() = x.ErrorMessage
+    end
+    
+type ParserResult<'a> = Success of 'a
+                      | Failure of ParserError   
+                      
+type ParserTempResult<'a> = 
+        | S of 'a * ParserInfo
+        | F of ParserError  
+
+type internal Parser<'a> = ParserInfo -> (ParserTempResult<'a>) seq
+  
+let internal yield' x = seq { yield x }
 
 /// Always returns a Success with x as result
 let value x : Parser<'a> =
-    fun s -> Success(x, s)
+    fun pi -> yield' <| S(x, pi)
 
 /// Always fails.
 let empty : Parser<'a> =
-    fun cs -> Failure cs
+    fun _ -> Seq.empty
+
+/// Bind operator. Applies f to the result of parser p.
+let (>>=) (p : Parser<'a>) (f : 'a -> Parser<'b>) : Parser<'b> =
+    let apply (f : 'a -> Parser<'b>) = 
+        function
+        | S(a, pi) -> (f a) pi
+        | F(e) -> yield' <| F(e)
+    fun pi -> Seq.concat (seq { for x in p pi do yield apply f x })
+
+/// Applies the first parser and if it fails, applies the second one.
+let (<|>) (p1 : Parser<'a>) (p2 : Parser<'a>) : Parser<'a> =
+    fun pi -> Seq.append (p1 pi) (p2 pi)
+
+/// Parses characters which satisfy the given function.
+let symf f : Parser<char> =
+    fun pi -> 
+        match pi.Rest with
+        | c::nrest when f c -> 
+            let nline = pi.Line + (if c = '\n' then 1 else 0)
+            let ncol = pi.Column + 1
+            yield' <| S(c, new ParserInfo(nrest, nline, ncol, pi.ExpectedValue))
+        | _ -> 
+            let exp = pi.ExpectedValue
+            let err = if exp.Length <> 0 then new ParserError(pi, exp, "") else new ParserError(pi, ParserError.SymfParserErrorMsg)
+            yield' <| F(err)
+
+/// Runs the given parser against the given string.
+let run (p : Parser<'a>) (s : string) = 
+    let results = p <| ParserInfo.StartInfo(s)
+    let succ = results
+               |> Seq.filter (fun x -> match x with S(_, pi) -> pi.IsSucceeded() | _ -> false)
+               |> Seq.map (fun (S(a, _)) -> a)
+    if Seq.isEmpty succ 
+    then 
+         Failure(ParserError.UnknownParserError)
+    else Success(Seq.head succ) 
 
 type ParserBuilder() =
     member x.Bind(p, f) = p >>= f
     member x.Return(y) = value y
 
 let parse = new ParserBuilder()
+
+/// Runs p as many times as posible, returning a list with the results.
+let rec many p : Parser<list<'a>> =
+    parse {
+        let! x = p
+        let! xs = many p
+        return x :: xs
+    } <|> value []
 
 /// If p succeeds returns the result x.
 let (>>%) p x : Parser<'b> =
@@ -46,13 +157,6 @@ let (.>>) p1 p2 : Parser<'a> =
 /// Applies p1 and p2 returning as a result a tuple with both results.
 let (.>>.) p1 p2 : Parser<'a * 'b> =
     p1 >>= (fun x -> p2 >>= (fun y -> value (x, y)))
-
-/// Applies the first parser and if it fails, applies the second one.
-let (<|>) (p1 : Parser<'a>) (p2 : Parser<'a>) : Parser<'a> =
-    fun s ->
-        match p1 s with
-        | Failure cs -> p2 s
-        | x -> x
     
 /// If p is successful applies f to the parsed element.
 let (|>>) p f : Parser<'b> =
@@ -61,14 +165,6 @@ let (|>>) p f : Parser<'b> =
 /// Runs phead and ptail, then create a list
 let (>|>>) phead ptail : Parser<'a list> =
     phead .>>. ptail >>= (fun (h, t) -> value (h :: t))
-
-/// Runs p as many times as posible, returning a list with the results.
-let rec many p : Parser<list<'a>> =
-    parse {
-        let! x = p
-        let! xs = many p
-        return x :: xs
-    } <|> value []
 
 /// Runs p as many times as possible with at least one Succcess.
 let many1 p : Parser<list<'a>> =
@@ -88,28 +184,21 @@ let rec times n p : Parser<list<'a>> =
 let any ps : Parser<'a> =
     Seq.reduce (fun p1 p2 -> p1 <|> p2) ps
 
-/// Runs the given parser against the given string.
-let run (p : Parser<'a>) (s : string) = p (List.ofArray <| s.ToCharArray())
-
 /// Tries to run p
 let opt (p : Parser<'a>) : Parser<'a option> = 
     p |>> Some <|> value None
+
+/// Tries to run p. If it success then apply f to result else return default value
+let optf f default' (p : Parser<'a>) = 
+    opt p |>> (fun x -> match x with Some y -> f y | None -> default')
 
 /// Applies the parsers popen, p and pclose in sequence. It returns the result of p.
 let between popen p pclose =
     popen >>. p .>> pclose
 
 /// Parses the given character.
-let sym c : Parser<char> =
-    function
-    | x::xs when x = c -> Success(x, xs)
-    | xs -> Failure xs
-    
-/// Parses characters which satisfy the given function.
-let symf f : Parser<char> =
-    function
-    | x::xs when f x -> Success(x, xs)
-    | xs -> Failure xs
+let sym c : Parser<char> = 
+    fun pi -> symf ((=) c) (new ParserInfo(pi, "symbol \'" + chars2str [c] + "\'"))
 
 /// Parses any of given character
 let syms cs : Parser<char> =
@@ -136,7 +225,7 @@ let pword = palpha
 let pwhitesp = many <| syms [' '; '\t'; '\n'; '\r']
 
 /// Makes p skip white space before its parsing
-let skipws p = pwhitesp >>. p .>> pwhitesp
+let skipws p = pwhitesp >>. p
 
 /// Parses the given char and skips white space characters.
 let symw c : Parser<char> = skipws (sym c)
@@ -162,11 +251,8 @@ let pfloat : Parser<float> =
     parse {
         let! sign = psign
         let! i = many1 pdigit
-        let! d = sym '.' >|>> many1 pdigit |> opt
-        let ends = match d with
-                   | Some x -> x
-                   | None -> []
-        return float (chars2str <| sign::(i @ ends))
+        let! d = sym '.' >|>> many1 pdigit |> optf id []
+        return float (chars2str <| sign::(i @ d))
     }
 
 /// Parses p splitted by c as many times as possible
--- sem4/ParserCombinator/Test.Core.fs
+++ sem4/ParserCombinator/Test.Core.fs
@@ -2,80 +2,92 @@
 open NUnit.Framework
 open ParserCombinators.Core
 
+let str2cs (s : string) = List.ofArray <| s.ToCharArray()
+let mkres value rest = [(value, str2cs rest)]
+let fails =
+    function
+    | Failure(_) -> true
+    | _ -> false
+
+
+type ``Basic parser operators``() =
+    [<Test>]
+    member x.``Value tests``() =
+        Assert.True(run (value 5) "" = Success(5))
+        Assert.True(run (value "abc") "" = Success("abc"))
+        Assert.True(run (value 9) "a" |> fails)
+
+    [<Test>]
+    member x.``Empty tests``() =
+        Assert.True(run empty "" |> fails)
+        Assert.True(run empty "abcd" |> fails)
+
+    [<Test>]
+    member x.``Symf tests``() =
+        Assert.True(run (symf ((=) 'a')) "a" = Success('a'))
+        Assert.True(run (symf ((<>) 'a')) "b" = Success('b'))
+        Assert.True(run (symf ((=) 'a')) "b" |> fails)
+        Assert.True(run (symf (fun c -> List.exists ((=) c) ['a'..'e'])) "d" = Success('d'))
+
+    [<Test>]
+    member x.``Sym tests``() =
+        Assert.True(run (sym 'a') "a" = Success('a'))
+        Assert.True(run (sym 'a') "b" |> fails)
+        Assert.True(run (sym 'a') "ax" |> fails)
+
+    [<Test>]
+    member x.``Operator (<|>) tests``() =
+        Assert.True(run (sym 'b' <|> sym 'a') "a" = Success('a'))
+        Assert.True(run (sym 'a' <|> sym 'b' <|> sym 'c') "a" = Success('a'))
+        Assert.True(run (sym 'a' <|> sym 'b' <|> sym 'c') "b" = Success('b'))
+        Assert.True(run (sym 'a' <|> sym 'b' <|> sym 'c') "c" = Success('c'))
+        Assert.True(run (sym 'b' <|> sym 'a') "cx" |> fails)
+
+    [<Test>]
+    member x.``Operator (>>=) tests``() =
+        Assert.True(run (sym 'b' >>= (fun _ -> value 4)) "b" = Success(4))
+        Assert.True(run (sym 'b' >>= (fun _ -> value 4)) "a" |> fails)
+        Assert.True(run (sym 'b' >>= (fun x -> sym x)) "bb" = Success('b'))
+        Assert.True(run (sym 'b' >>= (fun _ -> sym 'a')) "bx" |> fails)
+
+[<TestFixture>]
+type ``All parsers``() =
+    [<Test>]
+    member x.``Parser (between) tests``() =
+        Assert.True(run (between (sym '[') (many <| sym '+') (sym ']')) "[+]" = Success(['+']))
+
+    [<Test>]
+    member x.``Parser (opt) tests``() =
+        Assert.True(run (opt (sym 'a')) "a" = Success(Some 'a'))
+        Assert.True(run (opt (sym 'a')) "" = Success(None))
+        Assert.True(run (sym 'a' .>> opt (sym 'b')) "ab" = Success('a'))
+        Assert.True(run (sym 'a' .>> opt (sym 'b')) "a" = Success('a'))
+        Assert.True(run (sym 'a' .>> opt (sym 'b')) "ba" |> fails)
+
+    [<Test>]
+    member x.``Parser (syms) tests``() =
+        Assert.True(run (syms ['a';'b';'c']) "a" = Success('a'))
+        Assert.True(run (syms ['a';'b';'c']) "b" = Success('b'))
+        Assert.True(run (syms ['a';'b';'c']) "c" = Success('c'))
+        Assert.True(run (syms ['a';'b';'c']) "d" |> fails)
+    
 [<TestFixture>]
 type ``Integer parsing``() =
-    [<Test>]
-    member x.``Empty string should fail``() =
-        Assert.AreEqual((run (empty |>> int) ""), (run pint ""))
         
     [<Test>]
     member x.``One digit integers``() =
-        Assert.AreEqual(Success(0, []), (run pint "0"))
-        Assert.AreEqual(Success(1, []), (run pint "1"))
-        
-    [<Test>]
-    member x.``Multiple digit integers``() =
-        Assert.AreEqual(Success(123, []), (run pint "0123"))
-        Assert.AreEqual(Success(123, []), (run pint "123"))
-        
-    [<Test>]
-    member x.``Integer with signs``() =
-        Assert.AreEqual(Success(123, []), (run pint "+0123"))
-        Assert.AreEqual(Success(123, []), (run pint "+123"))
-        Assert.AreEqual(Success(-123, []), (run pint "-0123"))
-        Assert.AreEqual(Success(-123, []), (run pint "-123"))
-    
-    
-[<TestFixture>]
-type ``Float parsing``() =
-        
-    [<Test>]
-    member x.``Parsing zero``() =
-        Assert.AreEqual(Success(0.0, []), (run pfloat "0"))
-        Assert.AreEqual(Success(0.0, []), (run pfloat "-0"))
-        Assert.AreEqual(Success(0.0, []), (run pfloat "+0"))
-        Assert.AreEqual(Success(0.0, []), (run pfloat "-0.0"))
-        
-    [<Test>]
-    member x.``Parsing integers``() =
-        Assert.AreEqual(Success(123.0, []), (run pfloat "123"))
-        Assert.AreEqual(Success(123.0, []), (run pfloat "+123"))
-        Assert.AreEqual(Success(-123.0, []), (run pfloat "-123"))
-
-
-
-[<TestFixture>]
-type ``Operator tests``() =
-    [<Test>]
-    member x.``Test of (>>%) operator``() =
-        let p = (sym 'a') >>% 7
-        Assert.True((run p "a") = Success(7, []))
-        Assert.True((run pint "523") = Success(523, []))
+        Assert.True(run pdigit "7" = Success('7'))
+        Assert.True(run pdigit "0" = Success('0'))
+        Assert.True(run pdigit "9" = Success('9'))
+        Assert.True(run pdigit "a" |> fails)
 
     [<Test>]
-    member x.``Test of (>>.) operator``() =        
-        Assert.True(Success('b', []) = (run ((sym 'a') >>. (sym 'b')) "ab"))
-        Assert.True(Success(958, []) = (run ((sym 'a') >>. pint) "a958"))
-        Assert.True(Success('x', []) = (run ((many <| pstr "abc") >>. (sym 'x')) "abcabcx"))
-       // Assert.True((Failure) = (run ((sym 'a') >>. (sym 'b')) "ac"))
-       
-    [<Test>]
-    member x.``Test of (.>>) operator``() =        
-        Assert.True(Success('a', []) = (run ((sym 'a') .>> (sym 'b')) "ab"))
-        Assert.True(Success('a', []) = (run ((sym 'a') .>> pint) "a958"))
-        Assert.True(Success(5, []) = (run (((many <| pstr "abc") >>% 5) .>> (sym 'x')) "abcabcx"))
-        //Assert.True((Failure) = (run ((sym 'a') .>> (sym 'b')) "ac"))
-
-    [<Test>]
-    member x.``Test of (>|>>) operator``() =
-        let p = (sym 'a') >|>> (sym 'b' >>% ['x'])
-        Assert.True((run p "ab") = Success(['a';'x'], []))
-        //Assert.True((run p "b") = Failure)
-
-[<TestFixture>]
-type ``Primitive parser tests``() =
-    [<Test>]
-    member x.``Test of (pstr) operator``() =
-        let p = (pstr "abc") >>% 7
-        Assert.True((run p "abc") = Success(7, []))
-        //Assert.True((run p "abb") = Failure)
+    member x.``Int parsing``() =
+        Assert.True(run pint "42" = Success(42))
+        Assert.True(run pint "+42" = Success(42))
+        Assert.True(run pint "-7" = Success(-7))
+        Assert.True(run pint "0" = Success(0))
+        Assert.True(run pint "00" = Success(0))
+        Assert.True(run pint "0123" = Success(123))
+        Assert.True(run pint "-0" = Success(0))
+        Assert.True(run pint "3.14" |> fails)
