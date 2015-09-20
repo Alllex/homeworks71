11933d6da4c49e58b49a1c2938b21e09628f24cb
Aleksey Semin <alllex.semin@gmail.com>
2015-09-20 19:33:13 +0300

    Added: task3

---
+++ sem7/dotnet/task3/Drom/Drom/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Drom
+{
+
+    class PalindromDetector
+    {
+
+        private static bool IsDelimiter(char c)
+        {
+            return !Char.IsLetterOrDigit(c);
+        }
+
+        private static bool AreEqual(char a, char b)
+        {
+            return Char.ToLower(a).Equals(Char.ToLower(b));
+        }
+
+        public static bool IsPalindrom(string s)
+        {
+            int i = 0;
+            int j = s.Length - 1;
+            do
+            {
+                while (IsDelimiter(s[i])) i++;
+                while (IsDelimiter(s[j])) j--;
+                if (!AreEqual(s[i++], s[j--]))
+                {
+                    return false;
+                }
+            } while (i < j - 1);
+            return true;
+        }
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Type a string:");
+            string s = Console.ReadLine();
+            if (PalindromDetector.IsPalindrom(s))
+            {
+                Console.WriteLine("It is palindrom.");
+            } else {
+                Console.WriteLine("This string won't survive");
+            }
+        }
+    }
+}
---
+++ sem7/dotnet/task3/Drom/Drom/Tests.cs
@@ -0,0 +1,51 @@
+﻿using NUnit.Framework;
+
+namespace Drom
+{
+    [TestFixture]
+    public class Tests
+    {
+
+        private static bool IsPalindrom(string s)
+        {
+            return PalindromDetector.IsPalindrom(s);
+        }
+
+        [Test]
+        public void TestA()
+        {
+            Assert.IsTrue(IsPalindrom("A dog! A panic in a pagoda!"));
+            Assert.IsTrue(IsPalindrom("A man, a plan, a cat, a ham, a yak, a yam, a hat, a canal-Panama!"));
+            Assert.IsTrue(IsPalindrom("Are we not pure? “No sir!” Panama’s moody Noriega brags. “It is garbage!” Irony dooms a man; a prisoner up to new era."));
+        }
+
+        [Test]
+        public void TestG()
+        {
+            Assert.IsTrue(IsPalindrom("God’s dog."));
+        }
+
+        [Test]
+        public void TestO()
+        {
+            Assert.IsTrue(IsPalindrom("On a clover, if alive, erupts a vast, pure evil; a fire volcano."));
+        }
+
+        [Test]
+        public void TestP()
+        {
+            Assert.IsTrue(IsPalindrom("POW, ami! O’ Gad, ami! Go hang a salami, doc! Note; I dissent. A fast never prevents a fatness. I diet on cod. I’m a lasagna hog. I’m a dago! I’m a wop!"));
+        }
+
+
+        [Test]
+        public void TestTrueNegative()
+        {
+            Assert.IsFalse(IsPalindrom("abc"));
+            Assert.IsFalse(IsPalindrom("abcb"));
+            Assert.IsFalse(IsPalindrom("abc b b"));
+            Assert.IsFalse(IsPalindrom("aaaaaaaaaaaaaaacaaaaaaaaaaa"));
+            Assert.IsFalse(IsPalindrom("a b                   cxab a"));
+        }
+    }
+}
